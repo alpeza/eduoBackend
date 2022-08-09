a543aa65a7862376ab82d610ae161faf0e199239
alpeza06 <alpeza>
2022-08-09 17:21:04 +0200

    first

--- config/server.js
+++ config/server.js
@@ -1,8 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'https://alvarotestapp.herokuapp.com/',
   app: {
-    keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
+    keys: env.array('APP_KEYS'),
   },
 });
