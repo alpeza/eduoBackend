8d73c5b062386dd072e3761ebcf47a6095c3a580
alpeza06 <alpeza>
2022-08-10 00:03:28 +0200

    Authomatic commit

---
+++ src/api/portfolio-page/controllers/portfolio-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  portfolio-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::portfolio-page.portfolio-page');
---
+++ src/api/portfolio-page/routes/portfolio-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * portfolio-page router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::portfolio-page.portfolio-page');
