fa5e9814121a3c98f4601c3d5a3f950d59bc72af
alpeza06 <alpeza>
2022-08-09 17:04:18 +0200

    first

--- config/server.js
+++ config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'https://eduokitwebadmin.web.app/',
+  url: 'https://alvarotestapp.herokuapp.com/',
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
   },
 });
