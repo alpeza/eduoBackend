09dd3e3a0c950c1585186512c2ffecd23a28393c
alpeza06 <alpeza>
2022-08-10 18:15:29 +0200

    database

--- config/database.js
+++ config/database.js
@@ -1,16 +1,23 @@
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST', 'localhost'),
-      port: env.int('DATABASE_PORT', 54320),
-      database: env('DATABASE_NAME', 'postgres'),
-      user: env('DATABASE_USERNAME', 'user'),
-      password: env('DATABASE_PASSWORD', 'admin'),
-      ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        schema: env('DATABASE_SCHEMA', 'public'), // Not Required
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+        },
       },
-      ssl: env.bool('DATABASE_SSL', false),
+      options: {
+        ssl: env.bool('DATABASE_SSL', false),
+      },
     },
   },
 });
