cd092657f500bc854a827da9abdcf1701b1f50b7
alpeza06 <alpeza>
2022-08-16 21:40:41 +0200

    update

--- src/api/portfolio-page/controllers/portfolio-page.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  portfolio-page controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::portfolio-page.portfolio-page');
