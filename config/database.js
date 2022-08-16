b8977cee6dd9f48f50324b8c0eb3449e3e32ae8c
alpeza06 <alpeza>
2022-08-17 00:01:12 +0200

    update

--- config/database.js
+++ config/database.js
@@ -1,11 +1,15 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 54320),
+      database: env('DATABASE_NAME', 'postgres'),
+      user: env('DATABASE_USERNAME', 'user'),
+      password: env('DATABASE_PASSWORD', 'admin'),
+      ssl: {
+        rejectUnauthorized : false,
+      }
     },
-    useNullAsDefault: true,
   },
 });
