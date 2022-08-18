c744f9f395980fdfe135da78477adc0b992dc85e
alpeza06 <alpeza>
2022-08-18 13:23:09 +0200

    update

---
+++ src/api/collection-shop/routes/collection-shop.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * collection-shop router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::collection-shop.collection-shop');
