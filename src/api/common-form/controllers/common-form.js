0d2bfff7774f792021a221b21679da17ffb88ab5
alpeza06 <alpeza>
2022-08-28 20:11:35 +0200

    newform

---
+++ src/api/common-form/controllers/common-form.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  common-form controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::common-form.common-form');
