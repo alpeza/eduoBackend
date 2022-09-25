800147d672a765b49e1ed5cf11a8369acf0b31d4
alpeza <80060968+alpeza@users.noreply.github.com>
2022-09-25 18:12:43 +0200

    fix

--- config/database.js
+++ config/database.js
@@ -4,6 +4,7 @@ module.exports = ({ env }) => ({
     connection: {
       host: `/cloudsql/${env('INSTANCE_CONNECTION_NAME')}`,
       database: env('DATABASE_NAME'),
+      port: env('DATABASE_PORT'),
       user: env('DATABASE_USER'),
       password: env('DATABASE_PASSWORD'),
     },
