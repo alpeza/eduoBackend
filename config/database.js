4bd496b5441fadf96ed559b178f5b693f7cf5642
alpeza <80060968+alpeza@users.noreply.github.com>
2022-09-25 17:06:00 +0200

    fix

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: `/cloudsql/${env('INSTANCE_CONNECTION_NAME')}`,
+      host: `${env('INSTANCE_CONNECTION_NAME')}`,
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USER'),
       password: env('DATABASE_PASSWORD'),
